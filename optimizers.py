902fe83a7616d1c42d4d35d466d1df7fdec9ffa1
ltecot <lucastecot@gmail.com>
2023-07-12 13:08:28 -0700

    checkpoint

--- optimizers.py
+++ optimizers.py
@@ -6,8 +6,61 @@
 import torch
 import numpy as np
 
+class CustomSPSA():
+    """Customizable SPSA. Allows options for both 1st order and 2nd order modifier matrix
+        TODO: New class to make it Evolution-Strategy like (k-random pertubations instead of 2 symmetric ones)
+              Maybe make it even hill climbing / RSGF like (sample one random direction, compare it to current mean / point)"""
+
+    def __init__(self, param_len, num_shots=1, sampler=None, guider=None,       # General Params
+                 alpha=0.602, c=0.2, gamma=0.101, maxiter=None, A=None, a=None  # Plain SPSA params
+                 ): 
+        """
+        Arguments:
+            arg (type): description
+            TODO
+        """
+        self.param_len = param_len
+        self.num_shots = num_shots
+        self.c = c
+        self.gamma = gamma
+        self.alpha = alpha
+        self.A = A  
+        self.a = a  
+        if not A:
+            self.A = maxiter * 0.1
+        if not a:
+            self.a = 0.05 * (self.A + 1) ** alpha
+        self.sampler = sampler
+        self.guider = guider
+        self.t = 0  # Step count
+
+    def _eps_sampler(self):
+        if self.sampler == None:  # Default Bernoulli / Rademacher sampler for SPSA
+            return torch.distributions.bernoulli.Bernoulli(0.5 * torch.ones(self.param_len))
+        if self.sampler == "gaussian":
+            return torch.normal(mean=torch.zeros(self.param_len), std=1.0) 
+
+    def step(self, objective_fn, params, *args, **kwargs):
+        # Sampler
+        self.t += 1
+        ck = self.c / (self.t ** self.gamma)
+        grad_est = torch.zeros(self.n)
+        for i in range(self.num_shots):
+            eps = self._eps_sampler()
+            fp = objective_fn(params + torch.reshape(eps, params.shape), *args, **kwargs)
+            fn = objective_fn(params - torch.reshape(eps, params.shape), *args, **kwargs)
+            grad_est += (fp - fn) * (eps ** -1)
+        grad_est *= 1 / (2 * ck * self.num_shots)
+        ak = self.a / ((self.A + self.t) ** self.alpha)
+        return params - ak * torch.reshape(grad_est, params.shape)
+
+    def step_and_cost(self, objective_fn, params, *args, **kwargs):
+        loss = objective_fn(params, *args, **kwargs)
+        new_params = self.step(objective_fn, params, *args, **kwargs)
+        return new_params, loss
+
 class GES():
-    """Custom Implemented Guided Evolutionary Strategy"""
+    """Guided Evolutionary Strategy"""
 
     def __init__(self, param_len, lr, alpha, beta, variance, k, P):
         """
